848a10146432b1d3bc1fe49de241260bdceebb1f
Volodymyr <rozdolskyvolodymyr@gmail.com>
2019-01-30 12:46:57 -0500

    Added abstract class Auditable and extending it in Link & Comments

--- src/main/java/com/arwest/springit/domain/Comment.java
+++ src/main/java/com/arwest/springit/domain/Comment.java
@@ -6,16 +6,20 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Comment {
+public class Comment extends Auditable{
 
     @Id
     @GeneratedValue
     private Long id;
-
     private String body;
+
+    @ManyToOne
+    private Link link;
+
 
 }
--- src/main/java/com/arwest/springit/domain/Link.java
+++ src/main/java/com/arwest/springit/domain/Link.java
@@ -7,11 +7,14 @@ import lombok.NonNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Link {
+public class Link extends Auditable {
 
     @Id
     @GeneratedValue
@@ -20,5 +23,8 @@ public class Link {
     private String title;
     @NonNull
     private String url;
+
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 
 }
