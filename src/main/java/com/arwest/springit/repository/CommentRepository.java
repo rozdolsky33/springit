d939051aa7b111138b85e17d950c3a41abaf11a4
Volodymyr <rozdolskyvolodymyr@gmail.com>
2019-01-30 14:20:22 -0500

    Updated application properties with some basic h2 datasource setup

--- src/main/java/com/arwest/springit/repository/CommentRepository.java
+++ src/main/java/com/arwest/springit/repository/CommentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    Link findbyTitle(String title);
+    //Link findbyTitle(String title);
 }
